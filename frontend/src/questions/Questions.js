023c3c78b11e4a6d46ab286833e92011327162be
barakaganai <barakaganai01@gmail.com>
2023-02-03 11:43:38 +0300

    create questions.js

---
+++ frontend/src/questions/Questions.js
@@ -0,0 +1,91 @@
+import "./Questions.css";
+
+function Questions() {
+  return (
+    <div className="questions">
+      <div className="questionsTitle">
+        <h3 className="questionsHeading">Frequently Asked Questions</h3>
+      </div>
+      <div className="questionsBody">
+        <div className="questionsBodyItem">
+          <div className="questionsBodyItems">
+            <h6 className="questionsItemHeading">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est,
+              placeat voluptate. Aspernatur accusamus porro animi debitis. Quia
+              placeat corporis nostrum quos! Beatae, culpa aliquid quibusdam
+              consequatur iste autem soluta ratione?
+            </h6>
+            <i class="questionsIcon fa-solid fa-chevron-down"></i>
+          </div>
+          <div className="questionsParagraph">
+            <p className="questionsParagraphItem">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusamus, molestias rerum quas cupiditate voluptates dignissimos,
+              nobis repellendus veniam possimus, dolore explicabo corporis
+              aliquid consectetur delectus autem at iusto error sequi?
+            </p>
+          </div>
+        </div>
+        <div className="questionsBodyItem">
+          <div className="questionsBodyItems">
+            <h6 className="questionsItemHeading">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est,
+              placeat voluptate. Aspernatur accusamus porro animi debitis. Quia
+              placeat corporis nostrum quos! Beatae, culpa aliquid quibusdam
+              consequatur iste autem soluta ratione?
+            </h6>
+            <i class="questionsIcon fa-solid fa-chevron-down"></i>
+          </div>
+          <div className="questionsParagraph">
+            <p className="questionsParagraphItem">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusamus, molestias rerum quas cupiditate voluptates dignissimos,
+              nobis repellendus veniam possimus, dolore explicabo corporis
+              aliquid consectetur delectus autem at iusto error sequi?
+            </p>
+          </div>
+        </div>
+        <div className="questionsBodyItem">
+          <div className="questionsBodyItems">
+            <h6 className="questionsItemHeading">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est,
+              placeat voluptate. Aspernatur accusamus porro animi debitis. Quia
+              placeat corporis nostrum quos! Beatae, culpa aliquid quibusdam
+              consequatur iste autem soluta ratione?
+            </h6>
+            <i class="questionsIcon fa-solid fa-chevron-down"></i>
+          </div>
+          <div className="questionsParagraph">
+            <p className="questionsParagraphItem">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusamus, molestias rerum quas cupiditate voluptates dignissimos,
+              nobis repellendus veniam possimus, dolore explicabo corporis
+              aliquid consectetur delectus autem at iusto error sequi?
+            </p>
+          </div>
+        </div>
+        <div className="questionsBodyItem">
+          <div className="questionsBodyItems">
+            <h6 className="questionsItemHeading">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est,
+              placeat voluptate. Aspernatur accusamus porro animi debitis. Quia
+              placeat corporis nostrum quos! Beatae, culpa aliquid quibusdam
+              consequatur iste autem soluta ratione?
+            </h6>
+            <i class="questionsIcon fa-solid fa-chevron-down"></i>
+          </div>
+          <div className="questionsParagraph">
+            <p className="questionsParagraphItem">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusamus, molestias rerum quas cupiditate voluptates dignissimos,
+              nobis repellendus veniam possimus, dolore explicabo corporis
+              aliquid consectetur delectus autem at iusto error sequi?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Questions;
