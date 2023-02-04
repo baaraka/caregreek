cc8d6cb25caa347f96e0fa598edec0cd69e32312
barakaganai <barakaganai01@gmail.com>
2023-02-04 13:52:01 +0300

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
-import About from "./about/About";
 import "./App.css";
-import Home from "./home/Home";
-import Services from "./services/Services";
-import Team from "./team/Team";
+import Footer from "./footer/Footer";
 import TopBar from "./topBar/TopBar";
-import Why from "./why/Why";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <Home />
-      <About />
-      <Why />
-      <Services />
-      <Team />
+      <Footer />
     </div>
   );
 }
