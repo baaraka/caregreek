713969dcfcc574a33db73eb22f6ab02bcdfee083
barakaganai <barakaganai01@gmail.com>
2023-02-04 13:54:28 +0300

    create Service.js

---
+++ frontend/src/service/Service.js
@@ -0,0 +1,322 @@
+import "./Service.css";
+
+function Service() {
+  return (
+    <div className="service">
+      <div className="homeAboutUs">
+        <div className="homeAboutUsTitle">
+          <h3 className="homeAboutUsHeading">Our Services</h3>
+        </div>
+        <div className="homeAboutUsListDiv">
+          <ul className="homeAboutUsList">
+            <li className="homeAboutUsListItem">............</li>
+            <li className="homeAboutUsListItem">............</li>
+            <li className="homeAboutUsListItem">............</li>
+            <li className="homeAboutUsListItem">............</li>
+            <li className="homeAboutUsListItem">............</li>
+            <li className="homeAboutUsListItem">............</li>
+            <li className="homeAboutUsListItem">............</li>
+            <li className="homeAboutUsListItem">............</li>
+          </ul>
+        </div>
+      </div>
+      <div className="services">
+        <div className="servicesTitle">
+          <h3 className="servicesHeading">Our Services</h3>
+        </div>
+        <div className="serviceBody">
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+          <div className="divHeadingBody">
+            <div className="servicesIconDiv">
+              <span className="serviceBbn">
+                <i className="sIcon fa-solid fa-thumbs-up"></i>
+              </span>
+            </div>
+            <div className="servicesBodyHeading">
+              <h4 className="servicesBodyHeadingItem">Residential Cleaning</h4>
+              <p className="servicesParagraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+                quasi exercitationem. Odio perferendis minus, saepe, dolores
+                neque voluptates eaque amet impedit dignissimos dolorum
+                molestiae, aliquam excepturi itaque ratione a earum.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
