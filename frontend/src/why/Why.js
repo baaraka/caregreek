d4654a2cc18be31deef77e8231b88525db1f7f8f
barakaganai <barakaganai01@gmail.com>
2023-02-07 23:51:52 +0300

    add div

--- frontend/src/why/Why.js
+++ frontend/src/why/Why.js
@@ -5,33 +5,35 @@ function Why() {
     <div className="why">
       <div className="whyTitle">
         <h3 className="whyHeading">Why caregreek</h3>
-        <div className="whySpanDiv">
-          <span className="whyIconSpan">
-            <i className="whyIcon fa-solid fa-check"></i>
-          </span>
-          <p className="whyParagraph">
-            a superior clean through our trained staff and on-site supervision
-            to ensure accountability each and every time;
-          </p>
-        </div>
+        <div className="whySpanDivDiv">
+          <div className="whySpanDiv">
+            <span className="whyIconSpan">
+              <i className="whyIcon fa-solid fa-check"></i>
+            </span>
+            <p className="whyParagraph">
+              a superior clean through our trained staff and on-site supervision
+              to ensure accountability each and every time;
+            </p>
+          </div>
 
-        <div className="whySpanDiv">
-          <span className="whyIconSpan">
-            <i className="whyIcon fa-solid fa-check"></i>
-          </span>
-          <p className="whyParagraph">
-            a local operations team on-call 24 hours a day, available to work at
-            a moment's notice; and
-          </p>
-        </div>
-        <div className="whySpanDiv">
-          <span className="whyIconSpan">
-            <i className="whyIcon fa-solid fa-check"></i>
-          </span>
-          <p className="whyParagraph">
-            a safe “green” clean with the use of environmentally friendly
-            products, vacuums and microfiber dusting cloths.
-          </p>
+          <div className="whySpanDiv">
+            <span className="whyIconSpan">
+              <i className="whyIcon fa-solid fa-check"></i>
+            </span>
+            <p className="whyParagraph">
+              a local operations team on-call 24 hours a day, available to work
+              at a moment's notice; and
+            </p>
+          </div>
+          <div className="whySpanDiv">
+            <span className="whyIconSpan">
+              <i className="whyIcon fa-solid fa-check"></i>
+            </span>
+            <p className="whyParagraph">
+              a safe “green” clean with the use of environmentally friendly
+              products, vacuums and microfiber dusting cloths.
+            </p>
+          </div>
         </div>
       </div>
       <div className="whyVideoBody">
