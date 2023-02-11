33f371d7ff782a161dfc02060afea02716386d5a
barakaganai <barakaganai01@gmail.com>
2023-02-11 20:11:38 +0300

    update image

--- frontend/src/team/Team.js
+++ frontend/src/team/Team.js
@@ -14,7 +14,7 @@ function Team() {
       </div>
       <div className="teamImage">
         <img
-          src="http://caregreek.com/static/media/team.c23b6ddf20c31ba28605.png"
+          src="https://images.unsplash.com/photo-1579165466741-7f35e4755660?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           alt=""
           className="teamImageItem"
         />
