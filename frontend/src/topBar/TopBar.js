29f7d27a47289f767dd3670842bc4fdb9ed48ec9
barakaganai <barakaganai01@gmail.com>
2023-02-13 05:07:47 +0300

    update topBar.js

--- frontend/src/topBar/TopBar.js
+++ frontend/src/topBar/TopBar.js
@@ -1,125 +1,98 @@
 import { useState } from "react";
-import About from "../about/About";
-import AboutUs from "../aboutUs/AboutUs";
-import Contact from "../contact/Contact";
-import Gallery from "../gallery/Gallery";
-import Help from "../help/Help";
-import Home from "../home/Home";
-import Questions from "../questions/Questions";
-import Says from "../says/Says";
-import Service from "../service/Service";
-import Services from "../services/Services";
-import Team from "../team/Team";
-import Why from "../why/Why";
-
 import "./TopBar.css";
 
-function TopBar() {
-  const [currentPage, setCurrentPage] = useState("home");
+function TopBar(props) {
   const [show, setShow] = useState(false);
-
-  const handleNavigation = (page) => {
-    setCurrentPage(page);
-  };
-
   return (
-    <div>
-      <nav className="topBar">
-        <div className="leftTopBar">
-          <img
-            src="http://caregreek.com/static/media/logo.597684787fc988fd5ffb.png"
-            alt=""
-            className="leftBarImg"
-          />
-        </div>
-        <div className="centralTopBar">
-          <ul className="centralTopBarList">
+    <nav className="topBar">
+      <div className="leftTopBar">
+        <img
+          src="https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
+          alt=""
+          className="leftBarImg"
+        />
+      </div>
+      <div className="centralTopBar">
+        <ul className="centralTopBarList">
+          <l1
+            className="centralTopBarListItem"
+            onClick={() => props.handleNavigation("home")}
+          >
+            Home
+          </l1>
+          <l1
+            className="centralTopBarListItem"
+            onClick={() => props.handleNavigation("aboutUs")}
+          >
+            About Us
+          </l1>
+          <l1
+            className="centralTopBarListItem"
+            onClick={() => props.handleNavigation("services")}
+          >
+            Services
+          </l1>
+          <l1
+            className="centralTopBarListItem"
+            onClick={() => props.handleNavigation("contactUs")}
+          >
+            Contact
+          </l1>
+        </ul>
+      </div>
+      <div className="rightTopBar">
+        <button
+          onClick={() => props.handleNavigation("contactUs")}
+          className="rightTopBarBtn"
+        >
+          SHEDULE SERVICES
+        </button>
+      </div>
+      <div className="toggleMenu">
+        <i
+          onClick={() => setShow(!show)}
+          className="showBtn fa-solid fa-bars"
+        ></i>
+      </div>
+      {show ? (
+        <div className="sideTopBar">
+          <ul className="sideTopBarList">
             <l1
-              className="centralTopBarListItem"
-              onClick={() => handleNavigation("home")}
+              className="centralTopBarListItem list"
+              onClick={() => props.handleNavigation("home")}
             >
               Home
             </l1>
             <l1
-              className="centralTopBarListItem"
-              onClick={() => handleNavigation("aboutUs")}
+              className="centralTopBarListItem list"
+              onClick={() => props.handleNavigation("aboutUs")}
             >
               About Us
             </l1>
             <l1
-              className="centralTopBarListItem"
-              onClick={() => handleNavigation("services")}
+              className="centralTopBarListItem list"
+              onClick={() => props.handleNavigation("services")}
             >
               Services
             </l1>
             <l1
-              className="centralTopBarListItem"
-              onClick={() => handleNavigation("contactUs")}
+              className="centralTopBarListItem list"
+              onClick={() => props.handleNavigation("contactUs")}
             >
               Contact
             </l1>
+            <li className="centralTopBarListItems">
+              <button
+                onClick={() => props.handleNavigation("contactUs")}
+                className="rightTopBarBtn"
+              >
+                SHEDULE SERVICES
+              </button>
+            </li>
           </ul>
         </div>
-        <div className="rightTopBar">
-          <button className="rightTopBarBtn">SHEDULE SERVICES</button>
-        </div>
-        <div className="toggleMenu">
-          <i
-            onClick={() => setShow(!show)}
-            className="showBtn fa-solid fa-bars"
-          ></i>
-        </div>
-        {show ? (
-          <div className="sideTopBar">
-            <ul className="sideTopBarList">
-              <l1
-                className="centralTopBarListItem list"
-                onClick={() => handleNavigation("home")}
-              >
-                Home
-              </l1>
-              <l1
-                className="centralTopBarListItem list"
-                onClick={() => handleNavigation("aboutUs")}
-              >
-                About Us
-              </l1>
-              <l1
-                className="centralTopBarListItem list"
-                onClick={() => handleNavigation("services")}
-              >
-                Services
-              </l1>
-              <l1
-                className="centralTopBarListItem list"
-                onClick={() => handleNavigation("contactUs")}
-              >
-                Contact
-              </l1>
-              <li className="centralTopBarListItems">
-                <button className="rightTopBarBtn">SHEDULE SERVICES</button>
-              </li>
-            </ul>
-          </div>
-        ) : null}
-      </nav>
-      {currentPage === "home" && (
-        <div>
-          <Home />
-          <About />
-          <Why />
-          <Services />
-          <Team />
-          <Gallery />
-          <Help />
-          <Says />
-          <Questions />
-        </div>
-      )}
-      {currentPage === "aboutUs" && <AboutUs />}
-      {currentPage === "services" && <Service />}
-      {currentPage === "contactUs" && <Contact />}
-    </div>
+      ) : null}
+    </nav>
   );
 }
 
