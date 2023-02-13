1787c6c4966a77143b90ff29888492c533e57fe0
barakaganai <barakaganai01@gmail.com>
2023-02-13 04:59:57 +0300

    enable page navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,48 @@
-import "./App.css";
+import { useState } from "react";
 import Footer from "./footer/Footer";
 import TopBar from "./topBar/TopBar";
+import About from "./about/About";
+import AboutUs from "./aboutUs/AboutUs";
+import Contact from "./contact/Contact";
+import Gallery from "./gallery/Gallery";
+import Help from "./help/Help";
+import Home from "./home/Home";
+import Questions from "./questions/Questions";
+import Says from "./says/Says";
+import Service from "./service/Service";
+import Services from "./services/Services";
+import Team from "./team/Team";
+import Why from "./why/Why";
+import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const handleNavigation = (page) => {
+    setCurrentPage(page);
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div className="App">
-      <TopBar />
-      <Footer />
+      <TopBar handleNavigation={handleNavigation} />
+      {currentPage === "home" && (
+        <div>
+          <Home handleNavigation={handleNavigation} />
+          <About handleNavigation={handleNavigation} />
+          <Why />
+          <Services handleNavigation={handleNavigation} />
+          <Team />
+          <Gallery />
+          <Help />
+          <Says />
+          <Questions />
+        </div>
+      )}
+      {currentPage === "aboutUs" && <AboutUs />}
+      {currentPage === "services" && <Service />}
+      {currentPage === "contactUs" && <Contact />}
+      <Footer handleNavigation={handleNavigation} />
     </div>
   );
 }
